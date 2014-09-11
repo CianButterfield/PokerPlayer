cbc233f1d9057740fe983ec2a729f29435bec559
SeedPaths-User <lessdismissive@email.com>
2014-09-11 16:54:57 -0600

    Done by Both Cian and Drew

---
+++ PokerPlayer/PokerPlayer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BufferHeight = 10000;
+
+            Deck myDeck = new Deck();
+            myDeck.Shuffle();
+            myDeck.Shuffle();
+            myDeck.Shuffle();
+            myDeck.Shuffle();
+            myDeck.Shuffle();
+            myDeck.Shuffle();
+
+            PokerPlayer cian = new PokerPlayer();
+
+            for (int i = 0; i < 20; i++)
+            {
+                myDeck.Shuffle();
+                cian.DrawHand(myDeck.Deal(5));
+                cian.ShowHand();
+                Console.WriteLine();
+            }
+            
+
+
+            //keep console open
+            Console.ReadKey();
+        }
+    }
+}
